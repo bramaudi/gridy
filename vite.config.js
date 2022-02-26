1a5fd90e6d2665fe79c3b517c5b4f5a16761bc0a
Brama Udi <bramaudi.al@gmail.com>
2022-02-26 17:22:29 +0700

    Add PWA Assets

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,37 @@ import solidPlugin from "vite-plugin-solid"
 import tsconfigPaths from "vite-tsconfig-paths"
 import Pages from "vite-plugin-pages"
 import { VitePWA } from "vite-plugin-pwa"
-import PWAConfig from "./src/config/pwa"
 
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
-    VitePWA(PWAConfig),
+    VitePWA({
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],  
+      manifest: {
+        name: 'Gridy',
+        short_name: 'Gridy',
+        description: 'Add grid line overlay to image',
+        theme_color: '#fde68a',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          }
+        ]
+      }
+    }),
     Pages({
       react: true,
       importMode: 'async',
